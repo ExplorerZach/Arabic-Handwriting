d9c8ed5c8aeb92225b0018a4d8a8d5609987f185
Zach Z <zacharyziem@gmail.com>
2026-06-28 00:55:13 -0400

    bust sw cache for XP feature

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 // Bump this version string every time you deploy changes
-const CACHE = 'arabic-v74';
+const CACHE = 'arabic-v75';
 
 // All critical assets needed for the app to work
 const ASSETS = [
   '/',
   '/index.html',
-  '/assets/index-DgGGAOSe.js',
-  '/assets/index-CQ9LmHiE.css',
+  '/assets/index-5tLZgMRR.js',
+  '/assets/index-DoQxYrGh.css',
   '/manifest.json',
   '/icon-192.png',
   '/icon-512.png'
